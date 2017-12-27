34a2ea4e9b667391cc0ad8eb7de61668f7dfcd3a
Anibal Ambertin <aambertin@gmail.com>
2017-12-27 12:32:39 +0800

    Updated documentation on web-security module.

--- src/web-security.js
+++ src/web-security.js
@@ -1,11 +1,59 @@
-// TODO: how to add these decorators to work on class-level?
-// TODO: how to add these decorators to work on class-level with deny-first approach? (@Public / @AcceptPublic ?)
+/**
+ * Decorators for API Endpoint and Middleware Auth functions
+ * @module web-security
+ * @requires primavera/web
+ * @requires debug
+ * @requires lodash
+
+ */
 
 const debug = require('debug')('primavera:web-security')
 
 import _ from 'lodash'
 import { HttpError } from './web'
 
+/**
+ * Check if a given user possesses one of N authorized roles to execute this function.
+ * 
+ *
+ * @category web-security
+ * @name @RequiresRole
+ * @function
+ * @static
+ * @param  {...string} roles list of authorized roles.
+ * @param {function} [roleFetcher] function used to fetch roles from the authentication context.
+ * @param {function} [userFetcher] function used to fetch a user from the authentication context.
+ * @see RequiresAuth
+ * @example <caption>Must be ONE OF the following</caption>
+ * import { RequiresRole } from 'primavera/web-security'
+ * import { Controller, Route } from 'primavera/web'
+ * \@Controller()
+ * class MySecureController {
+ * 
+ *     \@RequiresRole('admin', 'manager')
+ *     \@Route.GET('users/list')
+ *     async listUsers() {
+ *         // ...
+ *     }
+ * 
+ * }
+ * 
+ * @example <caption>Must be ALL OF the following</caption>
+ * import { RequiresRole } from 'primavera/web-security'
+ * import { Controller, Route } from 'primavera/web'
+ * \@Controller()
+ * class MySecureController {
+ * 
+ *     \@RequiresRole('admin')
+ *     \@RequiesRole('manager')
+ *     \@Route.GET('users/list')
+ *     async listUsers() {
+ *         // ...
+ *     }
+ * 
+ * }
+ * 
+ */
 export function RequiresRole(roles, roleFetcher, userFetcher) {
     return function(target, name, descriptor) {
         if (!descriptor) throw new Error('@RequiresAuth can only be used on method-level.')
@@ -24,6 +72,56 @@ export function RequiresRole(roles, roleFetcher, userFetcher) {
     }
 }
 
+
+/**
+ * Check if the requestor is authenticated (user exists in request context).
+ * The _from_ argument can be a string or a function. If it's a string, the given attribute will be fetch
+ * from the request or session contexts.
+ * 
+ * In case as function is provided, the function is provided the controller as _this_ object,
+ * granting this way access to the controller or middleware class instance itself.
+ * 
+ * Additionally, the request and response objects will be provided to this function.
+ *
+ * @category web-security
+ * @name @RequiresAuth
+ * @function
+ * @static
+ * @param  {string|function} from where to fetch the user from
+ * @see RequiresAuth
+ * @example <caption>Fetch from [request|session] _user_ attribute.</caption>
+ * import { RequiresAuth } from 'primavera/web-security'
+ * import { Controller, Route } from 'primavera/web'
+ * \@Controller()
+ * class MySecureController {
+ * 
+ *     \@RequiresAuth('user')
+ *     \@Route.GET('me')
+ *     async me() {
+ *         // ...
+ *     }
+ * 
+ * }
+ * 
+ * @example <caption>Must be ALL OF the following</caption>
+ * import { RequiresAuth } from 'primavera/web-security'
+ * import { Controller, Route } from 'primavera/web'
+ * \@Controller()
+ * class MySecureController {
+ * 
+ *     \@RequiresAuth(myCustomAuthorizer)
+ *     \@Route.GET('users/list')
+ *     async listUsers() {
+ *         // ...
+ *     }
+ * 
+ * }
+ * 
+ * function myCustomAuthorizer(req, res) {
+ *     return req.user || req.session.user
+ * }
+ * 
+ */
 export function RequiresAuth(from = 'user') {
     return function(target, name, descriptor) {
         if (!descriptor) throw new Error('@RequiresAuth can only be used on method-level.')
